0c6666041b13cb95a8bd90293f71e7be4517493a
strayer_practice@outlook.com <strayer_practice@outlook.com>
2023-01-14 20:12:29 -0800

    Justin Marsh Week 2 lab2 Temperature Conversion

--- TemperatureConversion.py
+++ TemperatureConversion.py
@@ -1,1 +1,18 @@
+def to_celsius(fahrenheit):                 # Justin Marsh Week 2 lab2 Temperature Conversion
+    celsius = (fahrenheit - 32) * 5/9
+    return celsius
 
+def to_fahrenheit(celsius):
+    fahrenheit = celsius * 9/5 + 32
+    return fahrenheit
+
+# the main function is used to test the other functions
+
+
+if __name__ == "__main__":
+    for temp in range(0, 212, 40):
+        print(temp, "Fahrenheit =", round(to_celsius(temp), 2), "Celsius")
+
+    for temp in range(0, 101, 20):
+        print(temp, "Celsius =", round(to_fahrenheit(temp), 2),
+              "Fahrenheit")
